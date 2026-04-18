7116c83eccf7af13c487ec4b01a5c4dbe494905d
Enorian <enorian.rajo@gmail.com>
2026-04-18 14:42:45 +0200

    Add tooltip support for tagged slots

--- forge/forge-1.18.2/src/main/java/fr/en0ri4n/craftcreator/ClientSetup.java
+++ forge/forge-1.18.2/src/main/java/fr/en0ri4n/craftcreator/ClientSetup.java
@@ -1,21 +1,31 @@
 package fr.en0ri4n.craftcreator;
 
 import com.mojang.blaze3d.platform.InputConstants;
-import fr.en0ri4n.craftcreator.api.CCReferences;
+import fr.en0ri4n.craftcreator.api.ui.screen.TaggableSlotsContainerScreenDefinition;
 import fr.en0ri4n.craftcreator.impl.InitManager;
+import fr.en0ri4n.craftcreator.impl.blockentity.behaviors.TaggableSlotsBlockEntityBehavior;
 import fr.en0ri4n.craftcreator.platform.adapters.ForgeRegistryAdapter;
+import fr.en0ri4n.craftcreator.platform.blockentity.ForgeGenericBlockEntity;
+import fr.en0ri4n.craftcreator.platform.ui.container.ForgeRecipeCreatorMenu;
 import fr.en0ri4n.craftcreator.platform.ui.screen.ForgeRecipeCreatorScreen;
+import fr.en0ri4n.craftcreator.utils.Identifier;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.inventory.Slot;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(modid = CCReferences.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+import java.util.Map;
+import java.util.Optional;
+
+@Mod.EventBusSubscriber(modid = References.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientSetup
 {
     @SubscribeEvent
@@ -29,7 +39,7 @@ public class ClientSetup
                 InitManager.OPEN_MANAGEMENT_SCREEN_KEYBIND.getCategory()));
     }
 
-    @Mod.EventBusSubscriber(modid = CCReferences.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = References.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
     public static class KeyBindings
     {
         @SubscribeEvent
@@ -38,6 +48,43 @@ public class ClientSetup
             if(InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), InitManager.OPEN_MANAGEMENT_SCREEN_KEYBIND.getKeyCode()))
             {
                 InitManager.OPEN_MANAGEMENT_SCREEN_KEYBIND.getOnPress().run();
+            }
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = References.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+    public static class InventoryScreenHooks
+    {
+        @SubscribeEvent
+        public static void onTooltip(ItemTooltipEvent event)
+        {
+            if(event.getPlayer() == null) return;
+
+            if(event.getPlayer().containerMenu instanceof ForgeRecipeCreatorMenu menu)
+            {
+                if(Minecraft.getInstance().screen instanceof ForgeRecipeCreatorScreen screen)
+                {
+                    Slot slot = screen.getSlotUnderMouse();
+                    // Check if the slot belongs to our custom container and not the player inventory
+                    if(slot != null && slot.container instanceof ForgeGenericBlockEntity)
+                    {
+                        TaggableSlotsContainerScreenDefinition<? extends TaggableSlotsBlockEntityBehavior> taggableSlotsContainerScreenDefinition =
+                                (TaggableSlotsContainerScreenDefinition<? extends TaggableSlotsBlockEntityBehavior>) screen.getModel().getScreenDefinition();
+
+                        Map<Integer, Identifier> tags = taggableSlotsContainerScreenDefinition.getScreenData().getBehavior().getTaggedSlots();
+
+                        Optional<Integer> taggedSlot =  tags.keySet()
+                            .stream()
+                            .filter(index -> index == slot.index)
+                            .findFirst();
+
+                        if(taggedSlot.isPresent())
+                        {
+                            Identifier tagId = tags.get(taggedSlot.get());
+                            event.getToolTip().add(new TextComponent("Tag: " + tagId.toString()));
+                        }
+                    }
+                }
             }
         }
     }
